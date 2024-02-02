7dd59787ea05d74acd577033ff3fbe1096a370b4
chris <parweb@gmail.com>
2024-02-03 00:31:19 +0100

    proper link to login page

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -51,7 +51,7 @@ export const RegisterForm = () => {
     <CardWrapper
       headerLabel="Create an account"
       backButtonLabel="Already have an account?"
-      backButtonHref="/auth/login"
+      backButtonHref="/login"
       showSocial={false}
     >
       <Form {...form}>
