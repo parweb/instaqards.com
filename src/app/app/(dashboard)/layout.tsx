bd784793e5df197da357557b6e6fab072fb5040f
chris <parweb@gmail.com>
2024-03-31 01:32:28 +0100

    show PriceTable everywhere if not subscribed

--- src/app/app/(dashboard)/layout.tsx
+++ src/app/app/(dashboard)/layout.tsx
@@ -1,13 +1,30 @@
 import { ReactNode, Suspense } from 'react';
 
 import Nav from 'components/nav';
+import { PriceTable } from 'components/price-table';
 import Profile from 'components/profile';
+import { db } from 'helpers';
+import { getSubscription } from 'lib/auth';
 
 export default async function DashboardLayout({
   children
 }: {
   children: ReactNode;
 }) {
+  const subscription = await getSubscription();
+
+  const products = await db.product.findMany({
+    where: { active: { equals: true } },
+    include: {
+      prices: {
+        where: {
+          active: { equals: true },
+          interval_count: { equals: 1 }
+        }
+      }
+    }
+  });
+
   return (
     <div>
       <Nav>
@@ -15,7 +32,17 @@ export default async function DashboardLayout({
           <Profile />
         </Suspense>
       </Nav>
-      <div className="min-h-screen dark:bg-black sm:pl-60">{children}</div>
+
+      <div className="min-h-screen dark:bg-black sm:pl-60">
+        {!subscription.valid() && (
+          <div className="px-4 py-8">
+            {/* @ts-ignore */}
+            <PriceTable products={products} subscription={subscription} />
+          </div>
+        )}
+
+        <div className="">{children}</div>
+      </div>
     </div>
   );
 }
