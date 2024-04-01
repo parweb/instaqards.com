f1705078a5353f33f0ed7c1babc056321f723180
chris <parweb@gmail.com>
2024-04-01 23:39:23 +0200

    logo on authentification pages

--- src/app/app/(auth)/layout.tsx
+++ src/app/app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
 import { ReactNode, Suspense } from 'react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
+  const ratio = 1;
+
   return (
     <Suspense fallback={null}>
       <div className="flex min-h-screen">
@@ -9,7 +12,12 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
         </div>
 
         <div className="flex-1 hidden bg-muted lg:flex items-center justify-center">
-          <span className="text-9xl font-extrabold">Q</span>
+          <Image
+            src={`/rsz_black-transparent_withlink.png`}
+            alt="Logo qards.link"
+            width={800 * ratio}
+            height={400 * ratio}
+          />
         </div>
       </div>
     </Suspense>
