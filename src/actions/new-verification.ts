e3211ee847abce9caf08f393bab79e1982fd1f73
chris <parweb@gmail.com>
2024-04-04 17:02:56 +0200

    actions.new-password

--- src/actions/new-verification.ts
+++ src/actions/new-verification.ts
@@ -5,24 +5,25 @@ import { DEFAULT_LOGIN_REDIRECT } from 'auth.config';
 import { getUserByEmail } from 'data/user';
 import { getVerificationTokenByToken } from 'data/verificiation-token';
 import { db } from 'helpers/db';
+import { translate } from 'helpers/translate';
 
 export const newVerification = async (token: string) => {
   const existingToken = await getVerificationTokenByToken(token);
 
   if (!existingToken) {
-    return { error: 'Token does not exist!' };
+    return { error: translate('actions.new-verification.token.error') };
   }
 
   const hasExpired = new Date(existingToken.expires) < new Date();
 
   if (hasExpired) {
-    return { error: 'Token has expired!' };
+    return { error: translate('actions.new-verification.token.expire') };
   }
 
   const existingUser = await getUserByEmail(existingToken.email);
 
   if (!existingUser) {
-    return { error: 'Email does not exist!' };
+    return { error: translate('actions.new-verification.email.error') };
   }
 
   await db.user.update({
@@ -39,5 +40,5 @@ export const newVerification = async (token: string) => {
     redirectTo: DEFAULT_LOGIN_REDIRECT
   });
 
-  return { success: 'Email verified!' };
+  return { success: translate('actions.new-verification.email.form.success') };
 };
