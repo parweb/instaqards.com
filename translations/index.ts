1bff74f66efe57645620fde308538d47d3a14b77
chris <parweb@gmail.com>
2024-04-04 16:39:28 +0200

    translate actions/login

--- translations/index.ts
+++ translations/index.ts
@@ -7,6 +7,34 @@ const translations = {
     fr: 'Action interdite sur le serveur !',
     en: 'Forbidden Server Action!'
   },
+  'actions.login.validation.error': {
+    fr: 'Champs invalide !',
+    en: 'Invalid fields!'
+  },
+  'actions.login.validation.email.error': {
+    fr: "L'email n'existe pas !",
+    en: 'Email does not exist!'
+  },
+  'actions.login.validation.form.success': {
+    fr: 'Email de confirmation envoyé !',
+    en: 'Confirmation email sent!'
+  },
+  'actions.login.token.error': {
+    fr: 'Code invalide !',
+    en: 'Invalid code!'
+  },
+  'actions.login.token.expire': {
+    fr: 'Code expiré !',
+    en: 'Code expired!'
+  },
+  'actions.login.credentials.invalid': {
+    fr: 'Identifiants invalides !',
+    en: 'Invalid credentials!'
+  },
+  'actions.login.credentials.oops': {
+    fr: 'Un problème est survenu !',
+    en: 'Something went wrong!'
+  },
   'dashboard.home.title': {
     fr: 'Aperçu',
     en: 'Overview'
