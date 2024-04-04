bbc9570af4115a8034ef6615ee38683a6255a92a
chris <parweb@gmail.com>
2024-04-04 21:35:14 +0200

    prettier

--- src/app/[domain]/layout.tsx
+++ src/app/[domain]/layout.tsx
@@ -45,13 +45,6 @@ export async function generateMetadata({
     },
     icons: [logo],
     metadataBase: new URL(`https://${domain}`)
-    // Optional: Set canonical URL to custom domain if it exists
-    // ...(params.domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
-    //   data.customDomain && {
-    //     alternates: {
-    //       canonical: `https://${data.customDomain}`,
-    //     },
-    //   }),
   };
 }
 
@@ -69,7 +62,6 @@ export default async function SiteLayout({
     notFound();
   }
 
-  // Optional: Redirect to custom domain if it exists
   if (
     domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
     data.customDomain &&
