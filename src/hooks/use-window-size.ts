a275a3454a95431145b5a642a7b1ffdd44445084
chris <parweb@gmail.com>
2024-04-04 22:37:31 +0200

    prettier

--- src/hooks/use-window-size.ts
+++ src/hooks/use-window-size.ts
@@ -10,24 +10,18 @@ export default function useWindowSize() {
   });
 
   useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
+    const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight
       });
-    }
+    };
 
-    // Add event listener
-    window.addEventListener('resize', handleResize);
-
-    // Call handler right away so state gets updated with initial window size
     handleResize();
 
-    // Remove event listener on cleanup
+    window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+  }, []);
 
   return {
     windowSize,
