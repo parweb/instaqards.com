ecd0081915acb4708587654d80d9811a8d3afb10
chris <parweb@gmail.com>
2024-04-04 16:01:42 +0200

    translate content

--- src/actions/admin.ts
+++ src/actions/admin.ts
@@ -1,13 +1,14 @@
 'use server';
 
 import { currentRole } from 'helpers/auth';
+import { translate } from 'helpers/translate';
 
 export const admin = async () => {
   const role = await currentRole();
 
   if (role === 'ADMIN') {
-    return { success: 'Allowed Server Action!' };
+    return { success: translate('actions.admin.success') };
   }
 
-  return { error: 'Forbidden Server Action!' };
+  return { error: translate('actions.admin.error') };
 };
--- src/components/overview-stats.tsx
+++ src/components/overview-stats.tsx
@@ -2,6 +2,8 @@
 
 import { AreaChart, BadgeDelta, Card, Flex, Metric, Text } from '@tremor/react';
 
+import useTranslation from 'hooks/use-translation';
+
 export default function OverviewStats({
   chartdata,
   total
@@ -9,18 +11,24 @@ export default function OverviewStats({
   chartdata: { date: string; Clicks: number }[];
   total: number;
 }) {
+  const translate = useTranslation();
+
   return (
     <div className="grid gap-6 sm:grid-cols-2">
       <Card className="dark:!bg-stone-900">
-        <Text>Total Clicks</Text>
+        <Text>{translate('component.overview-stats.title')}</Text>
+
         <Flex
           className="space-x-3 truncate"
           justifyContent="start"
           alignItems="baseline"
         >
           <Metric className="font-cal">
-            {Intl.NumberFormat('us').format(total).toString()}
+            {Intl.NumberFormat({ en: 'us', fr: 'fr' }[translate.lang])
+              .format(total)
+              .toString()}
           </Metric>
+
           <BadgeDelta
             deltaType="moderateIncrease"
             className="dark:bg-green-900 dark:bg-opacity-50 dark:text-green-400"
@@ -28,12 +36,15 @@ export default function OverviewStats({
             34.3%
           </BadgeDelta>
         </Flex>
+
         <AreaChart
           className="mt-6 h-28"
           data={chartdata}
           index="date"
           valueFormatter={(number: number) =>
-            Intl.NumberFormat('us').format(number).toString()
+            Intl.NumberFormat({ en: 'us', fr: 'fr' }[translate.lang])
+              .format(number)
+              .toString()
           }
           categories={['Clicks', 'Visitors']}
           colors={['indigo', 'cyan']}
