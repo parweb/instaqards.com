6d1af7a7149bcdcd1feb4233cbee66122321bc58
chris <parweb@gmail.com>
2024-04-05 11:15:40 +0200

    more robust lang detector

--- src/hooks/use-translation.tsx
+++ src/hooks/use-translation.tsx
@@ -8,14 +8,31 @@ const useTranslation = () => {
     () => (Cookies.get('lang') || DEFAULT_LANG) as Lang
   );
 
+  let country = lang;
+
+  switch (true) {
+    case lang.toLowerCase().includes('fr'):
+      country = 'fr';
+      break;
+    case lang.toLowerCase().includes('en'):
+      country = 'en';
+      break;
+    case lang.toLowerCase().includes('us'):
+      country = 'en';
+      break;
+    default:
+      country = 'en';
+      break;
+  }
+
   const handle = (key: Part, options: Record<string, string> = {}) => {
     return Object.entries(options).reduce(
       (carry, [key, value]) => (carry || '').replaceAll(`{${key}}`, value),
-      (translations?.[key]?.[lang] ?? key) as string
+      (translations?.[key]?.[country] ?? key) as string
     );
   };
 
-  handle.lang = lang;
+  handle.lang = country;
   handle.setLang = (lang: Lang) => {
     Cookies.set('lang', lang);
     setLang(lang);
