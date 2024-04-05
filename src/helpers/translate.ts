b059d527ed7d5fc4cb8759ad7e55169b20505388
chris <parweb@gmail.com>
2024-04-05 11:15:40 +0200

    more robust lang detector

--- src/helpers/translate.ts
+++ src/helpers/translate.ts
@@ -6,7 +6,22 @@ export const translate = (
   key: Part,
   options: Record<string, string> = {}
 ): string => {
-  const lang = (cookies().get('lang')?.value || DEFAULT_LANG) as Lang;
+  let lang = (cookies().get('lang')?.value || DEFAULT_LANG) as Lang;
+
+  switch (true) {
+    case lang.toLowerCase().includes('fr'):
+      lang = 'fr';
+      break;
+    case lang.toLowerCase().includes('en'):
+      lang = 'en';
+      break;
+    case lang.toLowerCase().includes('us'):
+      lang = 'en';
+      break;
+    default:
+      lang = 'en';
+      break;
+  }
 
   return Object.entries(options).reduce(
     (carry, [key, value]) => carry.replaceAll(`{${key}}`, value),
