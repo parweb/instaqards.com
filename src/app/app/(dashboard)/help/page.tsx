cae0463ff82e49bcdc77bcc3d5a9baeed698e6c7
chris <parweb@gmail.com>
2024-04-05 12:59:40 +0200

    help page

---
+++ src/app/app/(dashboard)/help/page.tsx
@@ -0,0 +1,23 @@
+import { translate } from 'helpers/translate';
+
+export default async function HelpPage() {
+  return (
+    <div className="flex flex-col space-y-12 p-8">
+      <div className="flex flex-col space-y-6">
+        <div className="flex items-center justify-between">
+          <h1 className="font-cal text-3xl font-bold dark:text-white">
+            {translate('dashboard.help.title')}
+          </h1>
+        </div>
+
+        <div>
+          <div>{translate('dashboard.help.description')}</div>
+
+          <a href="tel:+1-303-499-7111" className="underline">
+            +1-303-499-7111
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
