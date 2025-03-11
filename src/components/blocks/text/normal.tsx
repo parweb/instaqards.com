2ea70452f8d729aac67dbce42ae51c7f822c8977
chris <parweb@gmail.com>
2025-03-12 00:24:29 +0100

    texts

--- src/components/blocks/text/normal.tsx
+++ src/components/blocks/text/normal.tsx
@@ -1,7 +1,11 @@
+import * as z from 'zod';
+
+export const input = z.object({
+  text: z.string().optional().describe('Text')
+});
+
 export default function NormalText({
   text = 'Normal text'
-}: {
-  text?: string;
-}) {
+}: z.infer<typeof input>) {
   return <div className="text-center">{text}</div>;
 }
