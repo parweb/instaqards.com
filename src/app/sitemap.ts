d899beeb1e7da050cf9f74825e51bd85483aae8f
chris <parweb@gmail.com>
2025-03-20 01:53:20 +0100

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,16 @@
-import { headers } from 'next/headers';
+import { db } from 'helpers/db';
 
 export default async function Sitemap() {
-  const headersList = headers();
-
-  const domain =
-    headersList
-      .get('host')
-      ?.replace(
-        '.localhost:11000',
-        `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
-      ) ?? 'qards.link';
+  const sites = await db.site.findMany({ orderBy: { updatedAt: 'desc' } });
 
   return [
     {
-      url: `https://${domain}`,
+      url: `https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
       lastModified: new Date()
-    }
+    },
+    ...sites.map(site => ({
+      url: `https://${site.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`,
+      lastModified: site.updatedAt
+    }))
   ];
 }
