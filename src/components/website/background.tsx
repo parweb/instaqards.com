880d27be0bbf359f58405ba5e2c79915e9683ae5
chris <parweb@gmail.com>
2025-04-04 12:02:24 +0200

    blur background on enter

--- src/components/website/background.tsx
+++ src/components/website/background.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import type { Site } from '@prisma/client';
+import { motion } from 'framer-motion';
 import { contentType } from 'mime-types';
 import Image from 'next/image';
-import React, { RefObject, Suspense } from 'react';
+import { RefObject, Suspense } from 'react';
 
 import { PreviewBackground } from 'components/editor/PreviewBackground';
 import { cn } from 'lib/utils';
@@ -22,59 +25,66 @@ export const Background = ({
     : contentType(background?.split('/').pop() ?? '') || '';
 
   return (
-    <div className="absolute inset-0 group pointer-events-auto">
-      {background && (
-        <>
-          {(media_type?.startsWith('video/') ||
-            media_type === 'application/mp4') && (
-            <>
-              <div className="absolute inset-0 bg-black/30 pointer-events-auto" />
+    <>
+      <motion.div
+        initial={{ filter: 'blur(100px)' }}
+        animate={{ filter: 'blur(0px)' }}
+        transition={{ duration: 1 }}
+        className="absolute inset-0 group pointer-events-auto"
+      >
+        {background && (
+          <>
+            {(media_type?.startsWith('video/') ||
+              media_type === 'application/mp4') && (
+              <>
+                <div className="absolute inset-0 bg-black/30 pointer-events-auto" />
 
-              <video
-                ref={videoRef}
-                className="absolute top-0 left-0 w-full h-full object-cover"
-                preload="metadata"
-                autoPlay={autoPlay}
-                loop
-                muted
-                playsInline
+                <video
+                  ref={videoRef}
+                  className="absolute top-0 left-0 w-full h-full object-cover"
+                  preload="metadata"
+                  autoPlay={autoPlay}
+                  loop
+                  muted
+                  playsInline
+                >
+                  <source src={background} type="video/mp4" />
+                </video>
+              </>
+            )}
+
+            {media_type?.startsWith('image/') && (
+              <div className="absolute top-0 left-0 w-full h-full object-cover">
+                <Image
+                  src={background}
+                  alt=""
+                  fill
+                  sizes="100vw"
+                  className="object-cover"
+                  priority
+                />
+              </div>
+            )}
+
+            {media_type === 'css' && (
+              <div
+                className={cn(
+                  `absolute top-0 left-0 w-full h-full object-cover`,
+                  { ' ': autoPlay === false && state === 'paused' }
+                )}
               >
-                <source src={background} type="video/mp4" />
-              </video>
-            </>
-          )}
+                <Suspense fallback={null}>
+                  <PreviewBackground name={background} />
+                </Suspense>
+              </div>
+            )}
+          </>
+        )}
 
-          {media_type?.startsWith('image/') && (
-            <div className="absolute top-0 left-0 w-full h-full object-cover">
-              <Image
-                src={background}
-                alt=""
-                fill
-                sizes="100vw"
-                className="object-cover"
-                priority
-              />
-            </div>
-          )}
-
-          {media_type === 'css' && (
-            <div
-              className={cn(
-                `absolute top-0 left-0 w-full h-full object-cover`,
-                { ' ': autoPlay === false && state === 'paused' }
-              )}
-            >
-              <Suspense fallback={null}>
-                <PreviewBackground name={background} />
-              </Suspense>
-            </div>
-          )}
-        </>
-      )}
-
-      {media_type === '' && (
-        <div className="absolute inset-0 bg-black/30 pointer-events-auto" />
-      )}
-    </div>
+        {media_type === '' && (
+          <div className="absolute inset-0 bg-black/30 pointer-events-auto" />
+        )}
+      </motion.div>
+    </>
   );
 };
