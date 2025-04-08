d540b5258f76a9a533ac93d1961816af30325af8
chris <parweb@gmail.com>
2025-04-08 15:52:03 +0200

    Identity Page

--- src/app/(marketing)/identity/client.tsx
+++ src/app/(marketing)/identity/client.tsx
@@ -5,7 +5,6 @@ import { DownloadIcon } from 'lucide-react';
 import { Button } from 'components/ui/button';
 
 export const DownloadButton = ({ src }: { src: string }) => {
-  // force download
   return (
     <Button
       onClick={() => {
