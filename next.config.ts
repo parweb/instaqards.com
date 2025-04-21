24a3e4c0a7d77d94cfecd03c7a91e63e25bc12d6
chris <parweb@gmail.com>
2025-04-21 22:20:35 +0200

    update deps

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: process.env.NODE_ENV === 'production' ? '.next' : '.next-dev',
   typescript: { ignoreBuildErrors: true },
   experimental: {
     // reactCompiler: true,
