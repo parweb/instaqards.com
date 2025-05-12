50f175256eccdcab80a9b0a3a7c4ce2a0a94deb6
parweb <parweb@gmail.com>
2025-05-12 09:41:37 +0200

    logout

--- src/components/auth/logout-button.tsx
+++ src/components/auth/logout-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { logout } from 'actions/logout';
+import { signOut } from 'lib/auth/client';
 
 interface LogoutButtonProps {
   children?: React.ReactNode;
@@ -8,7 +8,7 @@ interface LogoutButtonProps {
 
 export const LogoutButton = ({ children }: LogoutButtonProps) => {
   const onClick = () => {
-    logout();
+    signOut();
   };
 
   return (
