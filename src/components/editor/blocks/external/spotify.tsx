79222d44c4ae4efe23a14c93be72e690178a81b4
parweb <parweb@gmail.com>
2025-05-17 23:57:44 +0200

    block spotify accept all embed track|album|artist|playlist|show|episode

--- src/components/editor/blocks/external/spotify.tsx
+++ src/components/editor/blocks/external/spotify.tsx
@@ -2,26 +2,35 @@ import * as z from 'zod';
 
 import { json } from 'lib/utils';
 
+const regex =
+  /\/(track|album|artist|playlist|show|episode)\/([0-9A-Za-z]+)(?=\?|$)/gm;
+
 export const input = z.object({
   url: z
     .string()
-    .url()
-    .describe(json({ label: 'Track/Album URL', kind: 'string' }))
+    .regex(regex)
+    .describe(json({ label: 'Track/Album URL', kind: 'link', just: 'url' }))
 });
 
 export default function Spotify({
   url = 'https://open.spotify.com/intl-fr/album/2cWBwpqMsDJC1ZUwz813lo'
 }: Partial<z.infer<typeof input>>) {
-  const uri = url.split('/').slice(-2).join('/');
+  const [uri] = url.match(regex) ?? [];
+
+  // 80px (all)
+  // 152px (all)
+  // 232px (track, show, episode)
+  // 352px (all)
+  // > 352px fluid (album, artist, playlist)
 
   return (
     <iframe
+      className="!flex-none"
       title="Spotify"
-      style={{ borderRadius: '12px' }}
+      style={{ borderRadius: '6px' }}
       src={`https://open.spotify.com/embed/${uri}`}
       width="100%"
-      height="352"
-      frameBorder="0"
+      height="352px"
       allowFullScreen={true}
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
       loading="lazy"
