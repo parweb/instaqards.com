6aea9a42fc8e4c55fc9a406503912f88c5175ac1
parweb <parweb@gmail.com>
2025-05-19 11:22:31 +0200

    block instagram enforce url

--- src/components/editor/blocks/external/instagram.tsx
+++ src/components/editor/blocks/external/instagram.tsx
@@ -1,30 +1,33 @@
 'use client';
 
-import * as z from 'zod';
 import { InstagramEmbed } from 'react-social-media-embed';
+import * as z from 'zod';
 
+import { trySafe } from 'helpers/trySafe';
 import { json } from 'lib/utils';
 
 export const input = z.object({
   url: z
     .string()
     .url()
-    .describe(json({ label: 'Post URL', kind: 'string' }))
+    .describe(json({ label: 'Post URL', kind: 'link', just: 'url' }))
 });
 
-const Instagram = ({
-  url = 'https://www.instagram.com/p/DELQtvXpG7c'
-}: Partial<z.infer<typeof input>>) => {
-  const postId = url?.split('/').pop();
+const placeholder = 'https://www.instagram.com/p/DELQtvXpG7c/';
+const Instagram = ({ url = placeholder }: Partial<z.infer<typeof input>>) => {
+  [, url] = trySafe(() => {
+    const query = new URL(url);
+    return query.pathname.split('/').filter(Boolean).join('/');
+  }, placeholder);
 
   return (
-    <>
+    <div className="rounded-md overflow-hidden">
       <InstagramEmbed
-        url={`https://www.instagram.com/p/${postId}/`}
+        url={`https://www.instagram.com/${url}/`}
         captioned={false}
         width="100%"
       />
-    </>
+    </div>
   );
 };
 
